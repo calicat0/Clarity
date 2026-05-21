15bd6c1b72ee0f3e27540731a64d4eab55c29f9f
calicat0 <158124732+calicat0@users.noreply.github.com>
2026-05-21 21:17:40 +1000

    Add tutorial and implement one-time taks/notifications
    Adds tutorial level, and makes it default map.
    Makes it so that there is a one-time toggle for task/notification script entries.

--- maps/manifest.js
+++ maps/manifest.js
@@ -1,5 +1,5 @@
 mapManifest([
+    {name: "Tutorial", file: "Tutorial.mod.js"},
     {name: "World 1", file: "World_1.mod.js"},
-    {name: "Dev - World 1 (modular)", file: "world1.mod.js"},
     {name: "Dev - Lava Caves", file: "map2.js"}
 ]);
