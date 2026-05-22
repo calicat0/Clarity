201105ab1b3e8443f9a2c9c42bb0df76cccb0d0b
calicat0 <158124732+calicat0@users.noreply.github.com>
2026-05-22 18:24:58 +1000

    Add maps

--- maps/manifest.js
+++ maps/manifest.js
@@ -1,5 +1,6 @@
 mapManifest([
     {name: "Tutorial", file: "Tutorial.mod.js"},
+    {name: "Tutorial Advanced 1", file: "Tutorial_-_Advanced_1.mod.js"},
     {name: "World 1", file: "World_1.mod.js"},
-    {name: "Dev - Lava Caves", file: "map2.js"}
+    {name: "Challenge 1 Gauntlet", file: "Challenge_1_-_Gauntlet.mod.js"}
 ]);
