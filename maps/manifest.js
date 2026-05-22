72b9dffc0fe84e915cc43d0abd4f361b96188d28
calicat0 <158124732+calicat0@users.noreply.github.com>
2026-05-22 21:43:37 +1000

    User friendly editor changes, and add world 2
    easy add key, door and ui changes to editor add block tool
    add world 2

--- maps/manifest.js
+++ maps/manifest.js
@@ -2,5 +2,6 @@ mapManifest([
     {name: "Tutorial", file: "Tutorial.mod.js"},
     {name: "Tutorial Advanced 1", file: "Tutorial_-_Advanced_1.mod.js"},
     {name: "World 1", file: "World_1.mod.js"},
+    {name: "World 2", file: "World_2.mod.js"},
     {name: "Challenge 1 Gauntlet", file: "Challenge_1_-_Gauntlet.mod.js"}
 ]);
