a84873e9c2e76f9b24da6dc715d1929323ffefd3
calicat0 <158124732+calicat0@users.noreply.github.com>
2026-07-13 19:56:29 +1000

    Continue to edit Desert map, also ADD Desert map to the list (manifest)

--- maps/manifest.js
+++ maps/manifest.js
@@ -6,5 +6,6 @@ mapManifest([
     {name: "Trials 1", file: "Trials-1.mod.js"},
     {name: "Trials 2", file: "Trials-2.mod.js"},
     {name: "Cave 1", file: "Cave_1.mod.js"},
+    {name: "Desert", file: "Desert.mod.js"},
     {name: "Midnight", file: "midnight.js"}
 ]);
